6832740b5d6e7ab5aadfa4fc55624ae3e3702b6c
kawaiicakes <vomitmopper@gmail.com>
2025-10-29 20:15:32 -0600

    initial commit

---
+++ src/main/java/io/github/kawaiicakes/overgeared_attributes/OvergearedAttributes.java
@@ -0,0 +1,12 @@
+package io.github.kawaiicakes.overgeared_attributes;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(OvergearedAttributes.MOD_ID)
+public final class OvergearedAttributes {
+    public static final String MOD_ID = "overgeared_attributes";
+
+    public OvergearedAttributes() {
+
+    }
+}
