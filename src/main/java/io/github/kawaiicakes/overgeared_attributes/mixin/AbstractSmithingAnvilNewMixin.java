1fbbe362fdd102f466d5b985cc25ded70257f78e
kawaiicakes <vomitmopper@gmail.com>
2025-11-08 15:40:51 -0700

    better packet handling before i do some more radical changes

--- src/main/java/io/github/kawaiicakes/overgeared_attributes/mixin/AbstractSmithingAnvilNewMixin.java
+++ src/main/java/io/github/kawaiicakes/overgeared_attributes/mixin/AbstractSmithingAnvilNewMixin.java
@@ -4,8 +4,10 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.stirdrem.overgeared.block.custom.AbstractSmithingAnvilNew;
+import net.stirdrem.overgeared.networking.packet.PacketSendCounterC2SPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -26,10 +28,14 @@ public abstract class AbstractSmithingAnvilNewMixin extends BaseEntityBlock {
     private <MSG> void interruptPacketsForFreeHits(
             MSG message,
             Operation<Void> original,
+            @Local(argsOnly = true) LocalRef<BlockPos> pos,
             @Local(ordinal = 0) LocalRef<String> quality
     ) {
-        if (("skip").equals(quality.get())) return;
-        original.call(message);
+        final PacketSendCounterC2SPacket dispatchedMsg = ("skip").equals(quality.get())
+                ? new PacketSendCounterC2SPacket(pos.get(), "poor")
+                : (PacketSendCounterC2SPacket) message;
+
+        original.call(dispatchedMsg);
     }
 
     @WrapOperation(
